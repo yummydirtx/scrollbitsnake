568badc88afcd37f75adbcc1f85d77e5ebb94ae4
Alex Frutkin <61663391+yummydirtx@users.noreply.github.com>
2021-03-26 12:34:50 -0700

    comments

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ snakeLocation = [
 ]
 
 def drawSnake(snakeLocation):
+    # clear the screen before drawing
     scrollbit.clear()
+    # repeat this for every pair of coordinates
     for x in range(len(snakeLocation)/2):
+        # if x is length divided by two it has to be multiplied again
         scrollbit.set_pixel(snakeLocation[x*2], snakeLocation[(x*2)+1])
+    # show the world
     scrollbit.show()
 
 def moveSnake(snakeLocation, direction, length):
+    # if going to the right
     if direction == 0:
         snakeLocation.append(snakeLocation[len(snakeLocation)-2] + 1)
         snakeLocation.append(snakeLocation[len(snakeLocation)-2])
     while (len(snakeLocation) / 2) > length:
+        # remove extra snake
         snakeLocation = snakeLocation[2:]
     return snakeLocation
 
--- main.ts
+++ main.ts
@@ -3,20 +3,26 @@ let length = 1
 let direction = 0
 let snakeLocation = [0, 0]
 function drawSnake(snakeLocation: number[]) {
+    //  clear the screen before drawing
     scrollbit.clear()
+    //  repeat this for every pair of coordinates
     for (let x = 0; x < snakeLocation.length / 2; x++) {
+        //  if x is length divided by two it has to be multiplied again
         scrollbit.setPixel(snakeLocation[x * 2], snakeLocation[x * 2 + 1])
     }
+    //  show the world
     scrollbit.show()
 }
 
 function moveSnake(snakeLocation: number[], direction: number, length: number): number[] {
+    //  if going to the right
     if (direction == 0) {
         snakeLocation.push(snakeLocation[snakeLocation.length - 2] + 1)
         snakeLocation.push(snakeLocation[snakeLocation.length - 2])
     }
     
     while (snakeLocation.length / 2 > length) {
+        //  remove extra snake
         snakeLocation = snakeLocation.slice(2)
     }
     return snakeLocation
